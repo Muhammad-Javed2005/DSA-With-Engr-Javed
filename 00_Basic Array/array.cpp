b654936f9c591f0d14e1fd3f6e916f29ee4999eb
Muhammad-Javed2005 <muhammadjaved.tech5@gmail.com>
2026-03-06 10:42:08 +0500

    Array

---
+++ 00_Basic Array/array.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+#include <string>
+
+using namespace std;
+
+int main()
+{
+
+    int marks[5] = {45, 78, 98, 98, 65};
+
+    marks[3] = 34;
+    cout << marks[0] << endl;
+    cout << marks[1] << endl;
+    cout << marks[2] << endl;
+    cout << marks[3] << endl;
+    cout << marks[4] << endl;
+
+    cout << "This program is formed by Engineer Muhammad Javed";
+
+    return 0;
+}
