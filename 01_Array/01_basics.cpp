676b68490e0054ac7c964e8e70650515d59c8323
Muhammad-Javed2005 <muhammadjaved.tech5@gmail.com>
2026-03-11 22:53:56 +0500

    Basic

---
+++ 01_Array/01_basics.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+#include <climits>
+using namespace std;
+
+int main()
+{
+    int arr[5] = {10, 20, 30, 40, 50};
+
+    cout << "Array Elements: ";
+    for(int i = 0; i < 5; i++)
+        cout << arr[i] << " ";
+
+    cout << "\nProgram is developed by \"Engr. Muhammad Javed\"\n\n";
+    return 0;
+}
