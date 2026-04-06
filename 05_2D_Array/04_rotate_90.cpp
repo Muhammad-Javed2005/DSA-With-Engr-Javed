cd71fc00263ae52e334bf677df03f47a07ca560f
Muhammad-Javed2005 <muhammadjaved.tech5@gmail.com>
2026-04-06 23:21:44 +0500

    Rotate 90

---
+++ 05_2D_Array/04_rotate_90.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+#include <climits>
+using namespace std;
+
+int main()
+{
+    int matrix[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
+    int rotated[3][3];
+
+    // Rotate 90 degrees clockwise
+    for(int i = 0; i < 3; i++)
+        for(int j = 0; j < 3; j++)
+            rotated[j][3-1-i] = matrix[i][j];
+
+    cout << "Matrix rotated 90 degrees clockwise:\n";
+    for(int i = 0; i < 3; i++)
+    {
+        for(int j = 0; j < 3; j++)
+            cout << rotated[i][j] << " ";
+        cout << "\n";
+    }
+
+    cout << "\nProgram is developed by \"Engr. Muhammad Javed\"\n\n";
+    return 0;
+}
