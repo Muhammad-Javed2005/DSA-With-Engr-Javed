18dcdf5fbd7355afdbf51119fe424334ffc6c865
Muhammad-Javed2005 <muhammadjaved.tech5@gmail.com>
2026-04-23 22:42:03 +0500

    Insert After Value

---
+++ 11_Linked_List/Circular Linked List/05_insert_after_value.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+};
+Node *head = NULL;
+
+void InsertEnd(int val)
+{
+    Node *n = new Node{val, NULL};
+    if (!head)
+    {
+        n->next = n;
+        head = n;
+        return;
+    }
+    Node *ptr = head;
+    while (ptr->next != head)
+        ptr = ptr->next;
+    ptr->next = n;
+    n->next = head;
+}
+
+void InsertAfterValue(int target, int val)
+{
+    if (!head)
+        return;
+    Node *ptr = head;
+    do
+    {
+        if (ptr->data == target)
+        {
+            Node *n = new Node{val, ptr->next};
+            ptr->next = n;
+            return;
+        }
+        ptr = ptr->next;
+    } while (ptr != head);
+}
+
+void Display()
+{
+    if (!head)
+        return;
+    Node *ptr = head;
+    do
+    {
+        cout << ptr->data << " ";
+        ptr = ptr->next;
+    } while (ptr != head);
+    cout << endl;
+}
+
+int main()
+{
+    InsertEnd(10);
+    InsertEnd(20);
+    InsertEnd(30);
+    InsertAfterValue(20, 25);
+    cout << "After inserting 25 after 20: ";
+    Display();
+
+    cout << "\nProgram is developed by \"Engr. Muhammad Javed\"\n\n";
+    return 0;
+}
