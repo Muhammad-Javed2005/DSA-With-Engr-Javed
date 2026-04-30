3bf6e26e4a368c933eb47fe89cfb95f019569c37
Muhammad-Javed2005 <muhammadjaved.tech5@gmail.com>
2026-04-30 23:11:46 +0500

    Insert At Position

---
+++ 11_Linked_List/Singly Linked List/04_insert_position.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+};
+Node *head = NULL;
+
+void InsertHead(int val)
+{
+    Node *n = new Node{val, head};
+    head = n;
+}
+
+void InsertPosition(int pos, int val)
+{
+    if (pos == 1)
+    {
+        InsertHead(val);
+        return;
+    }
+    Node *ptr = head;
+    for (int i = 1; i < pos - 1 && ptr; i++)
+        ptr = ptr->next;
+    if (!ptr)
+        return;
+    Node *n = new Node{val, ptr->next};
+    ptr->next = n;
+}
+
+void Display()
+{
+    Node *ptr = head;
+    while (ptr)
+    {
+        cout << ptr->data << " ";
+        ptr = ptr->next;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    InsertHead(10);
+    InsertEnd(30);
+    InsertPosition(2, 20);
+    cout << "After inserting at position 2: ";
+    Display();
+
+    cout << "\nProgram is developed by \"Engr. Muhammad Javed\"\n\n";
+    return 0;
+}
+
+void InsertEnd(int val)
+{
+    Node *n = new Node{val, NULL};
+    if (!head)
+    {
+        head = n;
+        return;
+    }
+    Node *ptr = head;
+    while (ptr->next)
+        ptr = ptr->next;
+    ptr->next = n;
+}
