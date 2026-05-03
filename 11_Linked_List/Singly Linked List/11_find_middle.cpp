ec13e4653ddbc52733728c7ce15a2a07df27ebd3
Muhammad-Javed2005 <muhammadjaved.tech5@gmail.com>
2026-05-03 21:35:23 +0500

    Middle Note

---
+++ 11_Linked_List/Singly Linked List/11_find_middle.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+};
+Node *head = NULL;
+
+void InsertEnd(int val)
+{
+    Node *n = new Node{val, NULL};
+    if (!head)
+    {
+        head = n;
+        return;
+    }
+    Node *ptr = head;
+    while (ptr->next)
+        ptr = ptr->next;
+    ptr->next = n;
+}
+
+void FindMiddle()
+{
+    if (!head)
+        return;
+    Node *slow = head;
+    Node *fast = head;
+    while (fast && fast->next)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    cout << "Middle element: " << slow->data << endl;
+}
+
+void Display()
+{
+    Node *ptr = head;
+    while (ptr)
+    {
+        cout << ptr->data << " ";
+        ptr = ptr->next;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    InsertEnd(10);
+    InsertEnd(20);
+    InsertEnd(30);
+    InsertEnd(40);
+    InsertEnd(50);
+    cout << "Linked List: ";
+    Display();
+    FindMiddle();
+
+    cout << "\nProgram is developed by \"Engr. Muhammad Javed\"\n\n";
+    return 0;
+}
