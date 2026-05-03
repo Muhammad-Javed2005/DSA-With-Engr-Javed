9a714c484bec38e74bfd2fce5d44e36f9834e0e4
Muhammad-Javed2005 <muhammadjaved.tech5@gmail.com>
2026-05-03 21:35:03 +0500

    Reverse Single LL

---
+++ 11_Linked_List/Singly Linked List/10_reverse.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+};
+Node *head = NULL;
+
+void InsertEnd(int val)
+{
+    Node *n = new Node{val, NULL};
+    if (!head)
+    {
+        head = n;
+        return;
+    }
+    Node *ptr = head;
+    while (ptr->next)
+        ptr = ptr->next;
+    ptr->next = n;
+}
+
+void ReverseList()
+{
+    Node *prev = NULL;
+    Node *curr = head;
+    Node *next = NULL;
+    while (curr)
+    {
+        next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+    }
+    head = prev;
+}
+
+void Display()
+{
+    Node *ptr = head;
+    while (ptr)
+    {
+        cout << ptr->data << " ";
+        ptr = ptr->next;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    InsertEnd(10);
+    InsertEnd(20);
+    InsertEnd(30);
+    ReverseList();
+    cout << "After reversing: ";
+    Display();
+
+    cout << "\nProgram is developed by \"Engr. Muhammad Javed\"\n\n";
+    return 0;
+}
