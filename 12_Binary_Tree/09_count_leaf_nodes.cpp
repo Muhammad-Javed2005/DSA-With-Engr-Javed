167d04fbdd31691c0e361dc83d074818a786fb4d
Muhammad-Javed2005 <muhammadjaved.tech5@gmail.com>
2026-05-08 22:56:04 +0500

    Count Leaf Node

---
+++ 12_Binary_Tree/09_count_leaf_nodes.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *left;
+    Node *right;
+};
+Node *root = NULL;
+
+Node *Insert(Node *node, int val)
+{
+    if (!node)
+        return new Node{val, NULL, NULL};
+    if (val < node->data)
+        node->left = Insert(node->left, val);
+    else
+        node->right = Insert(node->right, val);
+    return node;
+}
+
+int CountLeaf(Node *node)
+{
+    if (!node)
+        return 0;
+    if (!node->left && !node->right)
+        return 1;
+    return CountLeaf(node->left) + CountLeaf(node->right);
+}
+
+int main()
+{
+    root = Insert(root, 10);
+    Insert(root, 5);
+    Insert(root, 20);
+    Insert(root, 15);
+
+    cout << "Leaf Nodes: " << CountLeaf(root) << endl;
+
+    cout << "\nProgram is developed by \"Engr. Muhammad Javed\"\n\n";
+    return 0;
+}
