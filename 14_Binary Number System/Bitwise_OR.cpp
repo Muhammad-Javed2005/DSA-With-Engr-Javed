adcafb5eaaaf05aed6c8d8713fba88df492d1342
Muhammad-Javed2005 <muhammadjaved.tech5@gmail.com>
2026-05-21 19:47:33 +0500

    Bitwise OR

---
+++ 14_Binary Number System/Bitwise_OR.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+
+    int a = 4;
+    int b = 8;
+
+    cout << (a | b) << endl;
+
+    cout << "This program is developed by \"Engr. Muhammad Javed\"" << endl;
+    return 0;
+}
+
