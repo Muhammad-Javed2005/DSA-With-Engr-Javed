c2876ec48e151c79f9eec4001af0793d89aabee7
Muhammad-Javed2005 <muhammadjaved.tech5@gmail.com>
2026-05-21 19:48:00 +0500

    Bitwise Right Shift

---
+++ 14_Binary Number System/Bitwise_Right_shift.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+
+    int a = 4;
+
+    cout << (8 >> 2) << endl;
+
+    cout << "This program is developed by \"Engr. Muhammad Javed\"" << endl;
+    return 0;
+}
