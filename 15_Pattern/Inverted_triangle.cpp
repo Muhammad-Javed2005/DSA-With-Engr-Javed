822ae8abb85841c795a9f8861555dbd933b73c06
Muhammad-Javed2005 <muhammadjaved.tech5@gmail.com>
2026-05-25 11:41:59 +0500

    Inverted Triangle

---
+++ 15_Pattern/Inverted_triangle.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+    int number;
+
+    cout << "Enter a size of pattern : ";
+    cin >> number; 
+
+    for (int i = 1 ; i <= number ; i++)
+    {
+        for (int  j  = number ; j >= i ; j--)
+        {
+            cout << i << " "; 
+        }
+        cout << endl;
+    }
+    
+
+    cout << "This program is developed by \"Engr. Muhammad Javed\"" << endl;
+    return 0;
+}
