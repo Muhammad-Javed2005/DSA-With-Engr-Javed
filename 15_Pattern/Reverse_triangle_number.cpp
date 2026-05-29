b899c8fd06cca459434841ba6f4d4586dbeb1371
Muhammad-Javed2005 <muhammadjaved.tech5@gmail.com>
2026-05-29 08:51:20 +0500

    Reverse Triangle Number

---
+++ 15_Pattern/Reverse_triangle_number.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+    int number;
+    cout << "Enter a size of pattern : ";
+    cin >> number; 
+
+    for (int i = 1; i <= number; i++)
+    {
+        int a = i;
+        for (int j = 1 ; j <= i ; j++)
+        {
+            
+            cout << a << " ";
+            a--;
+        }
+        cout << endl;
+
+    }
+    
+    cout << "This program is developed by \"Engr. Muhammad Javed\"" << endl;
+    return 0;
+}
